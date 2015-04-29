e98a453a4f22977e372f91353e712e0e2ee31aa4
kaiwangubnt <kai@ubnt.com>
2015-04-28 17:22:37 -0700

    more..

---
+++ Chapter1ArrayString/question1_53/Question.java
@@ -0,0 +1,34 @@
+/**
+    1.53    Design a method to find the frequency of occurrences of any given word in a book. 
+    (CareerCups150--17.9)
+ */
+
+package question1_53;
+
+import java.util.Hashtable;
+
+public class Question {
+
+    // Time: , Space:
+    public static int getFrequency(String word, Hashtable<String, Integer> dictionary){
+        
+        
+        
+        
+        return -1;
+    }
+    
+    public static Hashtable<String, Integer> setupDictionary(String[] book){
+        Hashtable<String, Integer> table = new Hashtable<String, Integer>();
+        
+        
+        return table;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
